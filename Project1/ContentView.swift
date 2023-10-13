35fcdacb985a2bb82303f7004b7652a73f92a8c4
emmajois <emmajoig@gmail.com>
2023-10-13 14:52:08 -0600

    testing integrations

--- Project1/ContentView.swift
+++ Project1/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello! Please work great")
+            Text("Testing Committing")
         }
         .padding()
     }
